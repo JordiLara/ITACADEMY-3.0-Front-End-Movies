928a717f803ec167ba8fcc8fa8282db483a21183
JordiLara <jordilarareyes@gmail.com>
2024-08-20 15:16:14 +0200

    add small changes for better understanding on variables for exercise 3

--- src/films.js
+++ src/films.js
@@ -1,33 +1,33 @@
 // Exercise 1: Get the array of all directors.
 
 function getAllDirectors(movies) {
-
-  return movies.map( movies => movies.director );
+  
+  return movies.map(movies => movies.director);
 }
 
 // Exercise 2: Get the films of a certain director
 
 function getMoviesFromDirector(movies, director) {
-  
-  return movies.filter( movies => movies.director == director );
+
+  return movies.filter(movies => movies.director == director);
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 
-function moviesAverageOfDirector(array, director) {
+function moviesAverageOfDirector(movies, director) {
 
-  let movies = array.filter( movie => movie.director === director );
-  let totalScore = movies.reduce((total, movie) => total + movie.score, 0);
-  let averageScore = totalScore / movies.length;
+  let moviesOfDirector = movies.filter( movie => movie.director === director );
+  let moviestotalScore = moviesOfDirector.reduce((total, movie) => total + movie.score, 0);
+  let moviesAverageScore = moviestotalScore / moviesOfDirector.length;
   
-  return parseFloat(averageScore.toFixed(2));
+  return parseFloat(moviesAverageScore.toFixed(2));
 }
 
 // Exercise 4:  Alphabetic order by title 
 
 function orderAlphabetically(movies) {
   
-  let moviesTitles = movies.map( movie => movie.title );
+  let moviesTitles = movies.map(movie => movie.title);
   let orderTitles = moviesTitles.sort();
   let firstTwentyTitles = orderTitles.slice(0, 20);
 
